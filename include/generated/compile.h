9e88ff7ba5de18b4cddc720d8c137cee51f79635
Skin1980 <skin1980@gmail.com>
2014-11-17 22:20:48 +0100

    2-way stereo call recording patch

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 10 */
+/* This file is auto generated, version 11 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#10 SMP PREEMPT Mon Nov 17 21:43:36 CET 2014"
+#define UTS_VERSION "#11 SMP PREEMPT Mon Nov 17 22:12:14 CET 2014"
 #define LINUX_COMPILE_BY "skin1980"
 #define LINUX_COMPILE_HOST "skin1980pc"
 #define LINUX_COMPILER "gcc version 4.8.4 20140719 (prerelease) (SaberMod-4.8.4_arm-graphite) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.4.0-perf-g767552d-dirty"
+#define UTS_RELEASE "3.4.0-perf-g0a7ba11-dirty"
