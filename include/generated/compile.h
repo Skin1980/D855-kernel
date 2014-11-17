34fd58c560e2f24d04cfb517576349b05ab2d468
Skin1980 <skin1980@gmail.com>
2014-11-17 07:31:53 +0100

    Upload source

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 3 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#3 SMP PREEMPT Sun Nov 16 22:53:30 CET 2014"
+#define LINUX_COMPILE_BY "skin1980"
+#define LINUX_COMPILE_HOST "skin1980pc"
+#define LINUX_COMPILER "gcc version 4.8.4 20140719 (prerelease) (SaberMod-4.8.4_arm-graphite) "
