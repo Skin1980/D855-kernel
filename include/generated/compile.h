40d78f4e60bd8ba08a623ba2047895d86f2e5e26
Skin1980 <skin1980@gmail.com>
2014-12-07 18:10:04 +0100

    Added simple_ondemand GPU governor by Faux123

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 31 */
+/* This file is auto generated, version 32 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#31 SMP PREEMPT Sun Dec 7 17:55:57 CET 2014"
+#define UTS_VERSION "#32 SMP PREEMPT Sun Dec 7 18:05:08 CET 2014"
 #define LINUX_COMPILE_BY "skin1980"
 #define LINUX_COMPILE_HOST "skin1980pc"
 #define LINUX_COMPILER "gcc version 4.8.4 20140719 (prerelease) (SaberMod-4.8.4_arm-graphite) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.4.0-perf-g41ff0e4-dirty"
+#define UTS_RELEASE "3.4.0-perf-ge340dbb-dirty"
