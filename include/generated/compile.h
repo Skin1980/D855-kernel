bf5400e6f90584c9941e54aadaec8858a74e8ba9
Skin1980 <skin1980@gmail.com>
2015-01-29 11:56:01 +0100

    Interactive governor fix

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
 /* This file is auto generated, version 99 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#99 SMP PREEMPT Thu Jan 8 11:14:15 CET 2015"
+#define UTS_VERSION "#99 SMP PREEMPT Thu Jan 29 11:47:27 CET 2015"
 #define LINUX_COMPILE_BY "skin1980"
-#define LINUX_COMPILE_HOST "skin1980pc"
-#define LINUX_COMPILER "gcc version 4.8.4 20140719 (prerelease) (SaberMod-4.8.4_arm-graphite) "
+#define LINUX_COMPILE_HOST "skin1980-550P5C-550P7C"
+#define LINUX_COMPILER "gcc version 4.9.3 20150113 (prerelease) (crosstool-NG 1.20.0 - Linaro GCC 2015.01) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.4.0-perf-g2e66eb2-dirty"
+#define UTS_RELEASE "3.4.0-perf-gad62d1f-dirty"
